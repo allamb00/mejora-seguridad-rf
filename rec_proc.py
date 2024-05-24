91cef01c13e40e2cfed641316911916cc290850c
allamb00 <114727358+allamb00@users.noreply.github.com>
2024-05-24 08:03:01 +0200

    Transmisión y recepción funcionales

--- rec_proc.py
+++ rec_proc.py
@@ -9,56 +9,95 @@ import struct
 import keyboard
 import time
 
-def replace_control_bytes(input_bytes):
-    # Reemplaza \x00 por '0' y \x01 por '1'
-    replaced_bytes = input_bytes.replace(b'\x00\x01', b'1').replace(b'\x00\x00', b'0')
-    return replaced_bytes
-
-def reduce_control_bytes(input_bytes):
-    output_bytes = b''
-    current_char = input_bytes[0]  # Inicializa el primer carácter
-    count = 0
-    for char in input_bytes:
-        if char == current_char:
-            count += 1
-        else:
-            for i in range(round(count/30)): #Soluciona la inconsistencia de 0s y 1s
-                output_bytes += bytes([current_char])
-            current_char = char
-            count = 1
-    # Agrega el último carácter al string reducido
-    output_bytes += bytes([current_char])
-    return output_bytes
+from collections import deque
 
 # Configura el socket UDP
 UDP_IP = "127.0.0.1"  # Dirección IP a la que GNU Radio está enviando los datos
 UDP_PORT = 2000  # Puerto al que GNU Radio está enviando los datos
-BUFFER_SIZE = 10240
+BUFFER_SIZE = 1472
 
 # Crea el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
 preamble = b'1001'
+
+# Inicializa un deque con una longitud máxima de 188 caracteres
+max_length = 200
+buffer = deque(maxlen=max_length)
+
+preamble = b'00000111010110111100110100010101' #Serial del fabricante para distinguir el código
+preamble_len = len(preamble)
+code_len = 188 #Tamaño total del código
+
+
+max_length = 200
+buffer = deque(maxlen=max_length)
+count = 0 #Variable global para que no se corte la cuenta entre payloads
+
+def process_bytes(byte_data):
+    code = b''
+    buffer.extend(byte_data)
+    if len(buffer) == max_length:
+        current_bytes = bytes(buffer)
+        
+        # Busca la subcadena que empieza con el preámbulo
+        for i in range(len(current_bytes) - preamble_len + 1):
+            if current_bytes[i:i+preamble_len] == preamble:
+                # Verifica si la cadena de 188 bytes está completa
+                if i + code_len <= len(current_bytes):
+                    code = current_bytes[i:i+code_len]
+                    print("\nCódigo válido encontrado:", code)
+                else:
+                    print(".", end = "")
+                break
+        else:
+            print(".", end = "")
+    return code
+
+def is_all_zeros(byte_data):
+    return all(b == ord('0') for b in byte_data)
+            
+def replace_control_bytes(input_bytes):
+    # Reemplaza \x00 por '0' y \x01 por '1'
+    replaced_bytes = input_bytes.replace(b'\x01\x00', b'1').replace(b'\x00\x00', b'0')
+    return replaced_bytes
+
+def reduce_control_bytes(input_bytes):
+    output_bytes = b''
+    current_char = input_bytes[0]  # Inicializa el primer carácter
+    global count
+    count = 0
+    for char in input_bytes:
+        
+        if char != current_char or count >= 30:                     
+            for i in range(round(count/30)): #Soluciona la inconsistencia de 0s y 1s
+                output_bytes += bytes([current_char])
+            current_char = char
+            count = 1
+        else:
+            count += 1
+    # Agrega el último carácter al string reducido  
+    if round(count/30) == 1 :
+        output_bytes += bytes([current_char])
+    
+    return output_bytes
+
 def main():
     # Bucle para recibir datos continuamente
     print("Escuchando...")
+    for a in range(0):
+        print(round(2/30))
     try:
         while True:
             data, addr = sock.recvfrom(BUFFER_SIZE)  # Recibe datos del socket
             # Procesa los datos recibidos (aquí puedes trabajar con los caracteres uchar)
             replaced = replace_control_bytes(data)
-            reduced = reduce_control_bytes(replaced)
-            # TEST
-            # print("data", data ) # Datos originales
-            # print("replaced", replaced ) # Datos convertidos a 0s y 1s
-            # print("reduced", reduced ) # Repeticiones reducidas. Mejora de consistencia
+            reduced = reduce_control_bytes(replaced)            
+             
+            code = process_bytes(reduced) #Busca el preámbulo en la entrada
+            #TODO ya tenemos el código completo que se ha enviado. Ahora hay que procesarlo
             
-            if reduced != b'0':                
-                print("Data: ", reduced)
-                                
-                if reduced.find(preamble):
-                    print("Código válido")
             """
             DECIPHER
             """
@@ -74,8 +113,7 @@ def main():
     
             print(timestamp)
             """
-            
-            
+
             
             
             
